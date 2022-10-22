926f90a64c8a3e20352c9b4f02000fffc1975a5f
Gozman <thegzmn@gmail.com>
2022-10-22 20:32:52 +0100

    Create index.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'main.js',
   },
   devServer: {
     static: {
