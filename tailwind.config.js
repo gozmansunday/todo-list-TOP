02d1ca58c62a5e5584e3ddf009c97d27cb06bf62
Gozman <thegzmn@gmail.com>
2022-11-12 18:32:48 +0100

    Update theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     './dist/**/*.{html,js}',
     './src/**/*.{html,js}',
   ],
+
+  important: true,
   
   theme: {
     screens: {
@@ -29,6 +31,13 @@ module.exports = {
         // 'card': '0 12px 12px -3px rgba(0, 0, 0, 0.3)',
         'card': '0px 5px 15px rgba(0, 0, 0, 0.35)',
       },
+    },
+    deliciousHamburgers: {
+      size: '30px', // must be in px.
+      color: '#586061',
+      colorLight: '#fff8f4',
+      padding: '0px', // must be in px.
+      animationSpeed: 1,
     },
   },
   plugins: [
