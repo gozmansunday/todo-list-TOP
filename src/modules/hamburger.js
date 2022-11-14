0bd3b63dee660152e3b314d31beb88bff5911ff5
Gozman <thegzmn@gmail.com>
2022-11-14 17:55:47 +0100

    Update logic

--- src/modules/hamburger.js
+++ src/modules/hamburger.js
@@ -2,54 +2,26 @@ import { dom } from './dom.js';
 
 function toggle() {
   const hamburger = dom.selector.hamburger;
-  const sideBarOverlay = dom.selector.sideBarOverlay;
   const sideBar = dom.selector.sideBar;
+  const sideBarOverlay = dom.selector.sideBarOverlay;
 
   hamburger.onclick = () => {
     hamburger.classList.toggle('active');
-    sideBar.classList.remove('side-bar-show');
-    sideBar.classList.remove('side-bar-hide');
-    sideBar.classList.remove('-translate-x-full');
-    sideBar.classList.remove('translate-x-0');
 
-    if ((hamburger.classList.contains('active'))) {
-      sideBarOverlay.classList.toggle('hidden');
-      sideBar.classList.toggle('side-bar-show');
+    // Side Bar Overlay
+    sideBarOverlay.classList.toggle('hidden');
+
+    // Side Bar
+    sideBar.classList.toggle('-translate-x-full');
+    sideBar.classList.toggle('translate-x-0');
+
+    if (sideBar.classList.contains('animate-show-side-bar') || sideBar.classList.contains('animate-hide-side-bar')) {
+      sideBar.classList.toggle('animate-hide-side-bar');
     }
 
-    if (!(hamburger.classList.contains('active'))) {
-      sideBar.classList.toggle('side-bar-hide');
-      // sideBar.classList.add('-translate-x-full');
-      sideBarOverlay.classList.toggle('hidden');
-    }
-
-  //   if ((hamburger.classList.contains('active'))) {
-  //     if (!(sideBar.classList.contains('side-bar-show')) && !(sideBar.classList.contains('side-bar-hide'))) {
-  //       sideBarOverlay.classList.toggle('hidden');
-  //       sideBar.classList.toggle('-translate-x-full');
-  //       sideBar.classList.toggle('side-bar-show');
-  //     }
-      
-  //     if ((sideBar.classList.contains('side-bar-hide'))) {
-  //       sideBarOverlay.classList.toggle('hidden');
-  //       sideBar.classList.toggle('-translate-x-full');
-  //       sideBar.classList.toggle('translate-x-0');
-  //       sideBar.classList.toggle('side-bar-hide');
-  //       sideBar.classList.toggle('side-bar-show');
-  //     }
-  //   } else {
-  //     sideBar.classList.toggle('side-bar-show');
-  //     sideBar.classList.toggle('side-bar-hide');
-  //     sideBarOverlay.classList.toggle('hidden');
-  //     sideBar.classList.toggle('-translate-x-full');
-  //     sideBar.classList.toggle('translate-x-0');
-  //   }
+    sideBar.classList.toggle('animate-show-side-bar');
   };
 }
-
-// function toggleSideBar(hamburger) {
-
-// }
 
 export const hamburger = {
   toggle,
