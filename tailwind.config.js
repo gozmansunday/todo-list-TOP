3e92872df9cb59c27681fe2eb219be681df4ff55
Gozman <thegzmn@gmail.com>
2022-12-01 23:57:23 +0100

    Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './dist/**/*.{html,js}',
-    './src/**/*.{html,js}',
-  ],
+  content: ['./dist/**/*.{html,js}', './src/**/*.{html,js}'],
 
   // important: true,
-  
+
   theme: {
     screens: {
       xs: '356px',
@@ -18,10 +15,10 @@ module.exports = {
     },
     extend: {
       colors: {
-        brand: "#15D155",
-        dark: "#222222",
-        mid: "#DADADA",
-        light: "#EEEEEE",
+        brand: '#15D155',
+        dark: '#222222',
+        mid: '#DADADA',
+        light: '#EEEEEE',
       },
       fontFamily: {
         spartan: ['League Spartan', 'sans-serif'],
@@ -43,11 +40,9 @@ module.exports = {
         },
       },
       boxShadow: {
-        'card': '0px 5px 15px rgba(0, 0, 0, 0.35)',
+        card: '0px 5px 15px rgba(0, 0, 0, 0.35)',
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-  ],
-}
+  plugins: [require('@tailwindcss/forms')],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   mode: 'development',
@@ -26,4 +26,4 @@ module.exports = {
       },
     ],
   },
-}
+};
